857353cf01bd2e3290bc3e22394069e8d8bf0ecf
Alison <ashelton@tamu.edu>
2015-12-27 14:27:13 -0800

    My solution for assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below allow for the cashing of matrix inversion.
+## This speeds up computation by preventing the need to repeatedly perfom inversion.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a matrix that is able to store its own inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) inv <<- solve(x)
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+
+
+## cacheSolve either returns the stored inverse or it calculates the inverse.
+
+cacheSolve  <- function(x, ...) {
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+      message("getting cached data")
+      return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
+  }
         ## Return a matrix that is the inverse of 'x'
-}
